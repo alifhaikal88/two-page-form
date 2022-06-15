e33cfb51ed92380f96de41908588b02bd7a8a421
Alif Haikal Razak <alifhaikal88@gmail.com>
2022-06-15 15:22:29 +0800

    update service url

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,17 +7,17 @@ import { Injectable } from '@angular/core';
 
 export class DataService {
 
-    url = 'http://localhost:3000/users' ;
+    url = 'https://nabilla-json-server.herokuapp.com/users';
 
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient) { }
 
     // used in second page to display data
-    getUsers(){
+    getUsers() {
         return this.http.get(this.url);
     }
 
     // used in first page's getData method. for the page to save the data that has been received 
-    serviceSaveData(data:any){
+    serviceSaveData(data: any) {
         console.log(data);
         return this.http.post(this.url, data);
     }
